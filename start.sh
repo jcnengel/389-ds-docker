8eafb86d8a0c3d152b501a25e9de8370c6668d4c
Johannes Engel <j.engel@intero-consulting.de>
2021-08-22 22:39:17 +0200

    Re-enable supervisord

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
-ns-slapd -D /etc/dirsrv/slapd-localhost -d 16984
+/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+#ns-slapd -D /etc/dirsrv/slapd-localhost -d 16984
